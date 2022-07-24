11a86d83679cb37c4af5f8a6acc8c25c2605b838
Philip Bridges <philipbr95@hotmail.com>
2022-07-24 21:49:59 +0100

    Missed checkin

---
+++ DapperExtensions.StrongName/Slapper/TypeConversions/FunctionTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static Slapper.AutoMapper.Configuration;
+
+namespace DapperExtensions.SlapperTypeConvertions
+{
+    public class FunctionTypeConverter : ITypeConverter
+    {
+        private readonly string _memberName;
+        private readonly Func<object, object> _converter;
+
+        public FunctionTypeConverter(string memberName, Func<object, object> converter)
+        {
+            _memberName = memberName;
+            _converter = converter;
+        }
+
+        public int Order => 1;
+
+        public bool CanConvert(object value, Type type, string memberName)
+        {
+            return string.IsNullOrWhiteSpace(memberName) == false && memberName.Equals(_memberName, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public object Convert(object value, Type type)
+        {
+            return _converter(value);
+        }
+    }
+}
